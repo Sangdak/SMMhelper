0e6492d61001b20fcef02f128c7a6564982d8abf
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-03-22 19:37:20 +1000

    Аdd communication with googlesheet via API/.

--- google_api.py
+++ google_api.py
@@ -1,12 +1,58 @@
 #  Взаимодействие с API гугл- таблиц и документов
 
-def get_tasks_from_gtable():
-    pass
+# def get_tasks_from_gtable():
+#     pass
+#
+#
+# def get_post_content_from_gdoc():
+#     pass
+#
+#
+# def get_post_img():
+#     pass
 
+from pprint import pprint
 
-def get_post_content_from_gdoc():
-    pass
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
 
+CREDENTIALS_FILE = 'creds.json'
+spreadsheet_id = '1fUgQWYFaqr8FuHIyJlrQp-ztrPpOugH9e0KJB6_aUSQ'
 
-def get_post_img():
-    pass
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive',
+    ]
+)
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+
+# values = service.spreadsheets().values().get(
+#     spreadsheetId=spreadsheet_id,
+#     range='A1:G7',
+#     majorDimension='ROWS'
+# ).execute()
+# pprint(values)
+# exit()
+
+values = service.spreadsheets().values().batchUpdate(
+    spreadsheetId=spreadsheet_id,
+    body={
+        "valueInputOption": "USER_ENTERED",
+        "data": [
+            {
+                "range": "B3:C4",
+                "majorDimension": "ROWS",
+                "values": [["This is B3", "This is C3"], ["This is B4", "This is C4"]]
+            },
+            {
+                "range": "D5:E6",
+                "majorDimension": "ROWS",
+                "values": [["This is D5", "This is D6"], ["This is E5", "=5+5"]]
+            }
+        ]
+    }
+).execute()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #  Основной файл для запуска приложения
 
-from google_api import get_tasks_from_gtable, get_post_content_from_gdoc, get_post_img
+#  from google_api import get_tasks_from_gtable, get_post_content_from_gdoc, get_post_img
 from poster import make_post_tg, make_post_ok, make_post_vk
 import time
 import os
